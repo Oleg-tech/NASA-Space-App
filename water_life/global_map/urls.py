f39112b0eab554d895d7df6142e0a78841dadc75
Olegurchick <64197137+Oleg-tech@users.noreply.github.com>
2023-10-07 09:44:13 +0300

    home page created

---
+++ water_life/global_map/urls.py
@@ -0,0 +1,7 @@
+from global_map.views import home
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('', home, name='home'),
+]
--- water_life/water_life/settings.py
+++ water_life/water_life/settings.py
@@ -5,7 +5,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-z^l*4*e9^xmc17fx4wc#jnt*75+=9pu=53cckhkht04*y7_bc8'
@@ -60,7 +59,6 @@ WSGI_APPLICATION = 'water_life.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -71,7 +69,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -90,7 +87,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -102,11 +98,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
